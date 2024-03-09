431e1bdb6be955a1c8d68a8833ec5ee1946faead
Colin Murphy <colmurph@adobe.com>
2024-03-09 12:00:23 -0500

    Working with 0.2.1

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,7 @@ use bindings::wasi::clocks;
 
 fn main() {
     let datetime = clocks::wall_clock::now();
-    println!("{:?}", datetime);
+    let timezone_display = clocks::timezone::display(datetime);
+
+    println!("{:?}", timezone_display.name);
 }
